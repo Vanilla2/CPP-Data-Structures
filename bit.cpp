595e0e42ab1a366bc9c099e185aadc4dbcaf5b9f
Vanilla2 <solmarians05@gmail.com>
2021-05-22 13:54:57 +0300

    Minor changes

--- bit.cpp
+++ bit.cpp
@@ -5,26 +5,24 @@ using namespace std;
 template <class T> class bit {
 
     private:
-        T get_bit(T x) {
-            return x & (-x);
-        }
-
         vector <T> arr;
+        int n;
 
     public: 
         bit(int maxval) {
+            n = maxval.size();
             arr.resize(maxval);
         }
 
-        void add(int pos, int val) {
-            for (int i = pos; i < arr.size(); i+=get_bit(i)) {
+        void update (int pos, int val) {
+            for (int i = pos; i < n; i+= (i & -i)) {
                 arr[i] += val;
             }
         }
 
-        int get_sum (int k) {
-            int s = 0;
-            for (int i = k; i>= 1; i-=get_bit(i)) {
+        T query (int k) {
+            T s = 0;
+            for (int i = k; i>= 1; i-= (i & -i)) {
                 s+=arr[i];
             }
             return s;
@@ -34,8 +32,8 @@ template <class T> class bit {
 int main(){
     // Example
     bit <int> d (500);
-    d.add(3, 5);
-    d.add(5, 5);
-    cout << d.get_sum(4) << " " << d.get_sum(6);
+    d.update(3, 5);
+    d.update(5, 5);
+    cout << d.query(4) << " " << d.query(6);
     return 0;
 }
